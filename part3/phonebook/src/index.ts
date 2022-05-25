c9496cdc9bfa25b6900f3de424e7bde2ed119c99
“celqaz” <819248392@qq.com>
2022-05-25 15:42:01 +0800

    [3] finish phonebook e3.1

--- part3/phonebook/src/index.ts
+++ part3/phonebook/src/index.ts
@@ -1,13 +1,37 @@
-import express from 'express';
-const app = express();
+import express, {Express, Request, Response} from 'express';
+
+const app: Express = express();
 app.use(express.json());
 
-const PORT = 3003;
+const PORT = 3001;
 
-app.get('/ping', (_req, res) => {
-    console.log('someone pinged here');
-    res.send('pong');
+
+const data = [
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+app.get('/persons', (_req: Request, res: Response) => {
+    res.json(data);
 });
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
