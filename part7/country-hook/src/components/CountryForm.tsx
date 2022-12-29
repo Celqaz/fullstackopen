2a3d1c99749c8a5f6481a6fe55493a8eef064b2b
“celqaz” <819248392@qq.com>
2022-12-29 11:11:11 +0800

    [p7] complete e7.7

--- part7/country-hook/src/components/CountryForm.tsx
+++ part7/country-hook/src/components/CountryForm.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useField, useCountry} from "../hooks";
 import {CountryType} from "../types/types";
 
 export default function CountryForm() {
 
     const searchInput = useField('text')
+    const [countryName,setCountryName] = useState<string>('')
 
-    const countryInfo: CountryType[] | null = useCountry(searchInput.value)
+    const countryInfo: CountryType[] | null = useCountry(countryName)
     const fetchData = (event: React.FormEvent) => {
         event.preventDefault();
         console.log('data is coming.', searchInput.value)
+        setCountryName(searchInput.value)
     }
 
-    console.log('countryInfo', countryInfo)
 
     return (
         <div>
@@ -20,12 +21,13 @@ export default function CountryForm() {
                 <input {...searchInput}/>
                 <button type={'submit'}>find</button>
             </form>
+            {!countryInfo[0] && <div>Country Info Not Found</div> }
             {countryInfo[0] &&
                 <div>
                     <h2>{countryInfo[0].name.common}</h2>
                     <div>capital: {countryInfo[0].capital}</div>
                     <div>{countryInfo[0].population}</div>
-                    <img src={countryInfo[0].flags.png} alt=""/>
+                    <img src={countryInfo[0].flags.png} height='100' alt={`flag of ${countryInfo[0].name.common}`}/>
                 </div>
             }
         </div>
